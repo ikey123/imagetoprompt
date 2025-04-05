bbef3a7a7e3469cc026d5c2c6dad09ab562e42d9
ikey123 <94415427@qq.com>
2025-04-05 20:16:28 +0800

    lang=en-us

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -4,10 +4,10 @@ import styles from './Layout.module.css';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className={styles.layout}>
+    <div className={styles.layout} lang="en-us">
       <Header />
       <main className={styles.main}>{children}</main>
       <Footer />
     </div>
   );
-} 
+}
